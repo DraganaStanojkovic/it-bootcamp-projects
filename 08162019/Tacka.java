08cb2adb9f23f7f22c4d1837c15b3b4443f751c3
Dragana Stanojkovic <54778787+DraganaStanojkovic@users.noreply.github.com>
2019-09-27 11:17:43 +0200

    Add files via upload

---
+++ 08162019/Tacka.java
@@ -0,0 +1,27 @@
+package klasaTrougaoTacka;
+
+public class Tacka {
+
+	private double x;
+	private double y;
+
+	public Tacka(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public String ispisTacke() {
+		String ispis = "";
+		ispis += "(" + x + ",-" + y + ")";
+		return ispis;
+
+	}
+}
